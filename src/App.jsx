4f8524aaec2e5a3f976711d47b83241ad9207ef4
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-06-27 11:10:36 +0530

    added footer to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
-    <>
+    <div className='app-wrapper'>
       <Header color="light" light={true}  expand="md" container="md" fixed="top" />
-    </>
+      <div  style={{marginTop:'100px'}}>
+        Some content
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <div className="footer mt-auto py-3 bg-body-tertairy">
+        <div className="container d-flex justify-content-center">
+            <span className="text-body-secondary">Shopcart</span>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
